7981a460b8c0911410779b557a19d2e808a2f67a
EtienneGomez <gomezb1701@alumno.ipn.mx>
2024-10-28 13:56:55 -0600

    cambio

--- src/main/java/com/ejemplo/registro/RegistroApplication.java
+++ src/main/java/com/ejemplo/registro/RegistroApplication.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RegistroApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(RegistroApplication.class, args);
+		SpringApplication.run(RegistroApplication.class args);
 	}
 }
