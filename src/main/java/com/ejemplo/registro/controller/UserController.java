22c397aa37254de0b04b4870638147de1a3ba299
Antonio Cano <antoniocncortazar@hotmail.com>
2024-10-31 18:44:06 -0600

    Integracion de registro con Facebook Backend

--- src/main/java/com/ejemplo/registro/controller/UserController.java
+++ src/main/java/com/ejemplo/registro/controller/UserController.java
@@ -1,12 +1,17 @@
 package com.ejemplo.registro.controller;
 
+import com.ejemplo.registro.model.FacebookUserRequest;
 import com.ejemplo.registro.model.GoogleUserRequest;
 import com.ejemplo.registro.model.User;
 import com.ejemplo.registro.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Date;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -42,6 +47,39 @@ public class UserController {
 
         userRepository.save(user);
         return "Registro con Google exitoso";
+    }
+
+    @PostMapping("/register/facebook")
+    public String registerUserWithFacebook(@RequestBody FacebookUserRequest facebookUserRequest) {
+        String accessToken = facebookUserRequest.getAccessToken();
+        String facebookGraphUrl = "https://graph.facebook.com/me?fields=id,name,email&access_token=" + accessToken;
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Map> response = restTemplate.getForEntity(facebookGraphUrl, Map.class);
+
+        if (response.getStatusCode() == HttpStatus.OK) {
+            Map<String, Object> userData = response.getBody();
+            String email = (String) userData.get("email");
+            String name = (String) userData.get("name");
+
+            // Verificar si el usuario ya existe por correo electrónico
+            Optional<User> existingUser = userRepository.findByCorreoUser(email);
+            if (existingUser.isPresent()) {
+                return "El usuario ya está registrado con Facebook";
+            }
+
+            // Crear un nuevo usuario con los datos de Facebook
+            User user = new User();
+            user.setCorreo_user(email);
+            user.setNombre_user(name);
+            user.setFecha_registro(new Date());
+            user.setRedSocial_ID_Social(2); // Asigna '2' para registro con Facebook
+
+            userRepository.save(user);
+            return "Registro con Facebook exitoso";
+        } else {
+            return "Token de Facebook no válido o expirado";
+        }
     }
 
     @GetMapping("/vivo")
---
+++ src/main/java/com/ejemplo/registro/model/FacebookUserRequest.java
@@ -0,0 +1,15 @@
+package com.ejemplo.registro.model;
+
+public class FacebookUserRequest {
+    private String accessToken;
+
+    // Getters y setters
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
+
